debb09944e5bcfa2cae8ef66196bdbec89b07c8e
Tiina Matala <t7mati01@students.oamk.fi>
2020-03-30 15:39:57 +0300

    countries show

--- CountriesProgrammatically/app/src/main/java/countries/programmatically/MainActivity.java
+++ CountriesProgrammatically/app/src/main/java/countries/programmatically/MainActivity.java
@@ -4,13 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> countries;
+    final String[] COUNTRIES = new String[] {
+            "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra",
+            "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina",
+            "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,5 +62,11 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listView = new ListView(this);
         mainLayout.addView(listView);
+        listView.setId(0);
+        ListView myListView = (ListView) findViewById(0);
+        final ArrayAdapter<String> aa;
+        aa = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,
+                COUNTRIES);
+        myListView.setAdapter(aa);
     }
 }
